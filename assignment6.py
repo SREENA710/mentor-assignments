f06134bec36eb2b7e4f2e8efbccf15dabea85794
Sreena <2459848@cognizant.com>
2026-06-12 12:00:05 +0530

    replaced code

--- assignment6.py
+++ assignment6.py
@@ -11,12 +11,33 @@
 #         print(f"'{s1}' and '{s2}' are not anagrams")
 
 
-s1 = input("Enter the string:")
-count = {}
-for word in s1:
-    if word in count:
-        count[word] += 1
-    else:
-        count[word] = 1
+
+from collections import Counter
+
+s1 = input("Enter the first string name: ")
+s2 = input("Enter the second string name: ")
+
+if Counter(s1) == Counter(s2):
+    print(f"'{s1}' and '{s2}' are anagrams")
+else:
+    print(f"'{s1}' and '{s2}' are not anagrams")
+
+
+
+# s1 = input("Enter the string:")
+# count = {}
+# for word in s1:
+#     if word in count:
+#         count[word] += 1
+#     else:
+#         count[word] = 1
+# for word, no_of_times in count.items():
+#     print(f"'{word}': {no_of_times} times")
+
+
+from collections import Counter
+
+s1 = input("Enter the string: ")
+count = Counter(s1)
 for word, no_of_times in count.items():
     print(f"'{word}': {no_of_times} times")
